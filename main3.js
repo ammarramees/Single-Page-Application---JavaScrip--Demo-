fbc70d4bf5e6d5349465b99451405462c371125e
Ammar Ramees <ammarramees@gmail.com>
2022-11-07 22:21:47 +0530

    push

---
+++ main3.js
@@ -0,0 +1,31 @@
+document.getElementById('features').style.display="none"
+document.getElementById('about').style.display="none"
+document.getElementById('price').style.display="none"  
+
+function homedisplay() {
+    document.getElementById('home').style.display="block"
+    document.getElementById('features').style.display="none"
+    document.getElementById('about').style.display="none"
+    document.getElementById('price').style.display="none"
+}
+
+function featuresdisplay() {
+    document.getElementById('features').style.display="block"
+    document.getElementById('home').style.display="none"
+    document.getElementById('about').style.display="none"
+    document.getElementById('price').style.display="none"
+}
+
+function aboutdisplay() {
+    document.getElementById('about').style.display="block"
+    document.getElementById('features').style.display="none"
+    document.getElementById('home').style.display="none"
+    document.getElementById('price').style.display="none"
+}
+
+function pricedisplay() {
+    document.getElementById('price').style.display="block"
+    document.getElementById('features').style.display="none"
+    document.getElementById('about').style.display="none"
+    document.getElementById('home').style.display="none"
+}
